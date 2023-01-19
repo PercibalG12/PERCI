44b613380590b303d7a722afccca9e53b665f1ea
Percival Roberts <percival.roberts13@gmail.com>
2023-01-18 21:47:25 -0500

    tried to update state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,10 @@ import Banner from "../assets/rep.jpg"
 import { useState } from "react";
 import "./main.css";
 const Main = () => {
-
+  const [className, setClassName] = useState('false')  // set class
+  const handleClick = () => {
+    setClassName('true') //Create a function that updates the state when the button is clicked. This function will change the class name to a new value.
+  }
   return (
     <div className='Banner false' 
     style={{
@@ -17,6 +20,7 @@ const Main = () => {
       transition:"transform 500ms cubic-bezier(.13,.53,.38,.97)",
       backgroundColor: "rgba(0,0,0)"
     }}>  
+   
 </div> 
   )
 }
--- src/components/Mainbtn.jsx
+++ src/components/Mainbtn.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./main.css";
 import { useState } from "react";
 
-const Mainbtn = () => {
+
+const Mainbtn = (props) => { // set props from main component
+
   return (
-    <button id="nav-toggle" type="button" > 
-  {/* onClick={handleClick} */}
+    <button id="nav-toggle" type="button" onClick={props.handleClick} > // change class name when clicked 
   <i className="open fa-light fa-bars-staggered" />
   <i className="close fa-light fa-xmark-large" />
+  
 </button>
   )
 }
